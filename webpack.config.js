405537e1078e52e07efa5bdc55e2de46cbb4cbba
kumata-san <kumatasan2019@gmail.com>
2019-11-30 11:45:46 +0900

    implement sass-loader

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,15 @@ module.exports = {
                 test: /\.css$/,
                 use: [
                     'style-loader',
-                    'css-loader'
+                    'css-loader',
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
                 ]
             },
             {
